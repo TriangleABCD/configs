17f6fac52e13b5bfcf3b55eab6449baaacfd7a57
Wang Yaoyong <yaoyong.oerv@isrc.iscas.ac.cn>
2024-09-20 15:21:35 +0800

    .

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,17 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- ---------- 正常模式 ---------- ---
+-- 窗口
+keymap.set("n", "<leader>sv", "<C-w>v") -- 水平新增窗口 
+keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
+
+-- 取消高亮
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- 切换buffer
+keymap.set("n", "<C-L>", ":bnext<CR>")
+keymap.set("n", "<C-H>", ":bprevious<CR>")
+
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")

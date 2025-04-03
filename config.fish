6df50242594854cadfc8734bdc46c61dec5bb202
wangyaoyong <yaoyong.oerv@isrc.iscas.ac.cn>
2025-04-03 22:50:25 +0800

    add tmux and fish

---
+++ config.fish
@@ -0,0 +1,29 @@
+set -g theme_display_git_default_branch yes
+set -g theme_git_default_branches master main
+set -g theme_color_scheme terminal-dark
+
+# function fish_greeting
+#     echo -e "\033[36m                                 "
+#     echo -e "           .---.                         " 
+#     echo -e "          /. ./|        ,---,       ,---," 
+#     echo -e "      .--'.  ' ;       /_ ./|      /_ ./|" 
+#     echo -e "     /__./ \ : | ,---, |  ' :,---, |  ' :" 
+#     echo -e " .--'.  '   \' ./___/ \.  : /___/ \.  : |" 
+#     echo -e "/___/ \ |    ' ' .  \  \ ,' '.  \  \ ,' '" 
+#     echo -e ";   \  \;      :  \  ;  `  ,' \  ;  `  ,'" 
+#     echo -e " \   ;  `      |   \  \    '   \  \    ' " 
+#     echo -e "  .   \    .\  ;    '  \   |    '  \   | " 
+#     echo -e "   \   \   ' \ |     \  ;  ;     \  ;  ; " 
+#     echo -e "    :   '  |--\"       :  \  \     :  \  \\" 
+#     echo -e "     \   \ ;           \  ' ;      \  ' ;" 
+#     echo -e "      '---\"             `--`        `--` "
+#     screenfetch
+# end
+
+# set -g fish_greeting
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+# eval /home/luoshen/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
